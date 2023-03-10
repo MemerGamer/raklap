82a1254b805b977497002906e4ae64d77e56399f
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2023-03-10 18:01:22 +0200

    wayland dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use smithay_client_toolkit::default_connect;
+use smithay_client_toolkit::window::{Window, WindowType};
+
 fn main() {
-    println!("Hello, world!");
+    // Initialize the Wayland connection
+    let (display, mut event_queue) = default_connect().expect("Failed to connect to Wayland server");
+
+    // Create a window
+    let window = Window::init(&display, WindowType::Basic).expect("Failed to create window");
 }
