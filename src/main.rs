e6163f1f64b9e34229add5a84840195202bf142f
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2023-03-10 18:04:39 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
-use smithay_client_toolkit::default_connect;
-use smithay_client_toolkit::window::{Window, WindowType};
+use smithay_client_toolkit::default_environment::Environment;
+use smithay_client_toolkit::window::SimpleWindow;
 
 fn main() {
     // Initialize the Wayland connection
-    let (display, mut event_queue) = default_connect().expect("Failed to connect to Wayland server");
+    let environment = Environment::init().unwrap();
+    let (display, mut event_queue) = environment.wayland_display();
 
     // Create a window
-    let window = Window::init(&display, WindowType::Basic).expect("Failed to create window");
+    let window = SimpleWindow::new(&display, 400, 300, None).unwrap();
+
+    // Display the window
+    window.set_title("Hello, world!");
+    window.map();
+
+    // Process events
+    event_queue.sync_roundtrip(&mut (), |_, _, _| {}).unwrap();
 }
+
